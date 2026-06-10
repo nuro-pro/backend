ff5f847f10b61687aa515851627235fa4a21f1d0
yooseungin <tmddls91@inha.edu>
2026-06-10 12:06:14 +0900

    [feat] Swagger 관련 config 파일 구성

---
+++ src/main/java/com/nuro/server/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.nuro.server.global.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(new Info()
+                        .title("nuro API")
+                        .description("nuro - AI 피부 진단 백엔드 API 명세서")
+                        .version("v1.0.0"));
+    }
+}
