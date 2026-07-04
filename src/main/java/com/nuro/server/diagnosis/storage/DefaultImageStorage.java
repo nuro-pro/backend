29712edfd9aa14e8e83f419ec20021998cc1080b
03mminseok <iris15977@gmail.com>
2026-07-05 02:18:47 +0900

    [feat] ci/cd 통과용 디폴트 이미지스토리지

---
+++ src/main/java/com/nuro/server/diagnosis/storage/DefaultImageStorage.java
@@ -0,0 +1,13 @@
+package com.nuro.server.diagnosis.storage;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.MimeType;
+
+@Component
+public class DefaultImageStorage implements ImageStorage {
+
+    @Override
+    public String store(byte[] bytes, MimeType mimeType) {
+        return "mock://image";
+    }
+}
