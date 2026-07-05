0e6ebb716bb05b16e063e672d847d972c4aefa74
03mminseok <iris15977@gmail.com>
2026-07-05 11:38:10 +0900

    defaultstorage profile 추가

--- src/main/java/com/nuro/server/diagnosis/storage/DefaultImageStorage.java
+++ src/main/java/com/nuro/server/diagnosis/storage/DefaultImageStorage.java
@@ -1,9 +1,11 @@
 package com.nuro.server.diagnosis.storage;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MimeType;
 
-//@Component
+@Component
+@Profile("test")
 public class DefaultImageStorage implements ImageStorage {
 
     @Override
