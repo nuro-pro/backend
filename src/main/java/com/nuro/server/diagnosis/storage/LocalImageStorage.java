5035c8977185d1c4125d35ef19a2193abc4c927b
03mminseok <iris15977@gmail.com>
2026-07-05 10:54:21 +0900

    localimagestorage profile(local) 삭제

--- src/main/java/com/nuro/server/diagnosis/storage/LocalImageStorage.java
+++ src/main/java/com/nuro/server/diagnosis/storage/LocalImageStorage.java
@@ -16,7 +16,6 @@ import java.util.UUID;
  * S3 연동 필요
  */
 @Component
-@Profile("local")
 public class LocalImageStorage implements ImageStorage {
 
     private static final String DIR = "uploads/diagnosis";
