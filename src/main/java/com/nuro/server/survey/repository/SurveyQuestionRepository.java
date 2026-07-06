fe8e2ea76c7dc57fdd1c5ac1aef4a5c76e7e30bf
03mminseok <iris15977@gmail.com>
2026-07-06 14:27:48 +0900

    [refactor] get surveyQuestion n+1 방지

--- src/main/java/com/nuro/server/survey/repository/SurveyQuestionRepository.java
+++ src/main/java/com/nuro/server/survey/repository/SurveyQuestionRepository.java
@@ -1,6 +1,7 @@
 package com.nuro.server.survey.repository;
 
 import com.nuro.server.survey.entity.SurveyQuestion;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 public interface SurveyQuestionRepository extends JpaRepository<SurveyQuestion, Long> {
     //active true인 모든 질문 조회
+    @EntityGraph(attributePaths = "answers")
     List<SurveyQuestion> findAllByDeletedAtIsNullOrderById();
 }
